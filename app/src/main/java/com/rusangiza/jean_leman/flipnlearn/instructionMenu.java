8ef843dea90affef7a55fac727cf3922b72924cc
Days0fThnder <j_rusangiza@yahoo.com>
2016-01-29 22:24:20 -0500

    intial commit, new main menu class.

---
+++ app/src/main/java/com/rusangiza/jean_leman/flipnlearn/instructionMenu.java
@@ -0,0 +1,14 @@
+package com.rusangiza.jean_leman.flipnlearn;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class instructionMenu extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instruction_menu);
+    }
+
+}
