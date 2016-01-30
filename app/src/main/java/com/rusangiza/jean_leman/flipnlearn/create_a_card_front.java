5b1110a22bc72c57703ea59d618c88fbc61bd53f
Days0fThnder <j_rusangiza@yahoo.com>
2016-01-29 23:56:41 -0500

    added the new create card page, where the user can create the front side of the page.

---
+++ app/src/main/java/com/rusangiza/jean_leman/flipnlearn/create_a_card_front.java
@@ -0,0 +1,14 @@
+package com.rusangiza.jean_leman.flipnlearn;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class create_a_card_front extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_a_card_front);
+    }
+
+}
--- app/src/main/java/com/rusangiza/jean_leman/flipnlearn/instructionMenu.java
+++ app/src/main/java/com/rusangiza/jean_leman/flipnlearn/instructionMenu.java
@@ -1,7 +1,10 @@
 package com.rusangiza.jean_leman.flipnlearn;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
 
 public class instructionMenu extends Activity {
 
@@ -9,6 +12,14 @@ public class instructionMenu extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instruction_menu);
+        Button createNewCardDeck = (Button) findViewById(R.id.create_new_deck_btn);
+        createNewCardDeck.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View V) {
+                     startActivity(new Intent("com.rusangiza.jean_leman.flipnlearn.create_a_card_front"));
+                 }
+             }
+        );
     }
 
 }
